95b8f8b48383c64716b1d8e9dfeb30368af8f8da
Lyu Ji <hkfhfkcoc@protonmail.com>
2025-01-06 17:27:36 -0600

    minor

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,8 @@ export const links: Route.LinksFunction = () => [
 		crossOrigin: "anonymous",
 	},
 	{
-		rel: "stylesheet",
+		// Preload google font
+		rel: "preload",
 		href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
 	},
 	{ rel: "stylesheet", href: stylesheet },
